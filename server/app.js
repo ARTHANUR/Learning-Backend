ac0b07644161b0bc2003a64763900dc27722c2e7
shashank <sarthanur@gmail.com>
2024-05-26 18:36:41 +0530

    secured DB connection code using dotenv , also created seperate file for connection

--- server/app.js
+++ server/app.js
@@ -1,35 +1,31 @@
-const mongoose = require("mongoose")
-const express = require("express")
+const dotenv = require("dotenv");
+const express = require("express");
 const app = express();
+dotenv.config({ path: "./config.env" });
+require("./db/conn.js");
 
-const DB = `mongodb+srv://sarthanur:12345@cluster0.z4gwwqy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const PORT = process.env.PORT;
 
-mongoose.connect(DB).then(()=>{
-    console.log("connection successfull")
-}).catch((err)=>{
-    console.log("no connection")
-})
+app.get("/", (req, res) => {
+    res.send("this is HOME page");
+});
 
-app.get("/",(req,res)=>{
-    res.send("this is HOME page")
-})
+app.get("/signup", (req, res) => {
+    res.send("this is SIGNUP page");
+});
 
-app.get("/signup",(req,res)=>{
-    res.send("this is SIGNUP page")
-})
+app.get("/signin", (req, res) => {
+    res.send("this is LOGIN page");
+});
 
-app.get("/signin",(req,res)=>{
-    res.send("this is LOGIN page")
-})
+app.get("/about", (req, res) => {
+    res.send("this is ABOUT page");
+});
 
-app.get("/about",(req,res)=>{
-    res.send("this is ABOUT page")
-})
+app.get("/contact", (req, res) => {
+    res.send("this is CONTACT page");
+});
 
-app.get("/contact",(req,res)=>{
-    res.send("this is CONTACT page")
-})
-
-app.listen(3000,()=>{
-    console.log("server is up and running at 3000")
-})
+app.listen(PORT, () => {
+    console.log(`server is up and running at ${PORT}`);
+});
